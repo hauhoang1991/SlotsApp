40918b173628bd59ea417e7a6de78e0a9b46e8ea
Hau Hoang <hauhoangduc@gmail.com>
2022-09-08 10:54:06 -0700

    Initial Commit

---
+++ SlotsApp/SlotsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotsAppApp.swift
+//  SlotsApp
+//
+//  Created by Hau Hoang on 9/8/22.
+//
+
+import SwiftUI
+
+@main
+struct SlotsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
